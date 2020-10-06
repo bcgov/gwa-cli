2e0d6a08ed3bec0a8133f55abb8d48a0933bf235
Joshua Jones <joshua@general-metrics.com>
2020-10-06 01:20:28 -0700

    Fix some errors

--- src/validators/key-auth.ts
+++ src/validators/key-auth.ts
@@ -1,7 +1,9 @@
 import * as Joi from 'joi';
 
 const keyAuthSchema = Joi.object({
-  key_names: Joi.array(Joi.string().pattern(/^[a-zA-Z0-9_-]+$/)).optional(),
+  key_names: Joi.array()
+    .items(Joi.string().pattern(/^[a-zA-Z0-9_-]+$/))
+    .optional(),
   key_in_body: Joi.boolean().optional(),
   hide_credentials: Joi.boolean().optional(),
   anonymous: Joi.string().empty('').optional(),
