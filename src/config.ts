a0f03dc2c181d543c9efe2a0c3abcd0990c2a671
Joshua Jones <joshua@general-metrics.com>
2020-12-11 15:35:13 -0800

    Add update scripts, sync with 1.1.0

--- src/config.ts
+++ src/config.ts
@@ -4,15 +4,15 @@ type Config = {
 
 function config(): Config {
   const namespace: string = process.env.GWA_NAMESPACE || '';
-  const env = process.env.GWA_ENV || '';
+  const env: string = process.env.GWA_ENV || '';
   const clientId: string = process.env.CLIENT_ID || '';
   const clientSecret: string = process.env.CLIENT_SECRET || '';
   const legacyAuthorizationEndpoint: string =
     'https://auth-qwzrwc-test.pathfinder.gov.bc.ca/auth/realms/aps/protocol/openid-connect/token';
   const legacyApiHost: string =
     'https://gwa-api-qwzrwc-test.pathfinder.gov.bc.ca/v1';
-  const authorizationEndpoint: string = `https://authz-264e6f-${env}.apps.silver.devops.gov.bc.ca/auth/realms/aps/protocol/openid-connect/token`;
-  const apiHost: string = `https://gwa-api-264e6f-${env}.apps.silver.devops.gov.bc.ca/v1`;
+  const authorizationEndpoint: string = `https://authz-apps-gov-bc-ca.${env}.apsgw.xyz/auth/realms/aps/protocol/openid-connect/token`;
+  const apiHost: string = `https://gwa-api-gov-bc-ca.${env}.apsgw.xyz/v1`;
 
   return {
     authorizationEndpoint,
